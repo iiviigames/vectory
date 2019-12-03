94c26da09aef97570815ed987255a4526e4e04a7
iivii <iiviigames@pm.me>
2019-12-02 19:10:19 -0500

    :speaker:
    Added some helpers.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Vectory
 This is just a little project in Castle aiming to further my understanding of [vector math](http://immersivemath.com/ila/ch02_vectors/ch02.html).
 
 Additionally, I am terrible at trigonometry and need to get better at it.
-![Unit Circle](https://d2jmvrsizmvf4x.cloudfront.net/G3kA6SNHT2rgY068tllw_0DD3310F-BDFA-4A9F-96C4-3DEC51608A71.png)
+---
+
+![This makes my head spin](https://upload.wikimedia.org/wikipedia/commons/9/9a/Degree-Radian_Conversion.svg)
---
+++ helpers.lua
@@ -0,0 +1,85 @@
+--	HELPERS.LUA
+--	=================================================================
+
+local help = {}
+
+--	Smooth Motion
+	---@func 	approach(shift,limit,rate)
+	---@arg		{flt}	shift	value to change over time
+	---@arg		{flt}	limit	maximum amount the shift can reach
+	---@arg		{flt}	rate	incremental value * delta per step
+	---@desc	The approach script smoothly moves one value towards
+	---				a maximum amount without exceeding that amount.
+	---@ret		{flt}	shift	the altered shifting value
+function approach(shift,limit,rate)
+	if shift < limit then
+		shift += rate;
+
+		if shift > limit then
+			return limit
+		end
+	else
+		shift -= rate;
+
+		if shift < limit then
+			return limit
+		end
+	end
+
+	return shift
+end
+
+
+--	Line Division
+	---@func 	div(symbol, newline)
+	---@arg		{str}	symbol	string value for a separator to draw on screen
+	---@arg		{int}	newline	-1 gives a newline before sep, 1 gives a newline after
+	---@desc	Creates a line division in text on screen.
+function div(s,nl)
+	local s = s or '█'
+	local b = '\r'
+	local n = nl or 0
+	if (nl == -1) p(b)
+	for i=1, 4 do
+		s = s..s
+	end
+	p(s)
+	if (nl == 1) p(b)
+end
+
+--	Table Print
+	---@func 	pt(t)
+	---@arg		{tbl}	t		table to print to screen
+	---@desc	Prints a table of values to the screen
+function pt(t)
+	for k,v in pairs(t) do
+		print(ts(k)..":		"..ts(v))
+	end
+end
+
+--	Table Reverse
+function reverse(t)
+	local nt = {}
+	local l = #t + 1
+	for k,v in ipairs(t) do
+		nt[l - k] = v
+	end
+	return nt
+end
+
+-- Decimal to Binary
+function bin(num)
+	local t = {}
+	local rest
+	while num > 0 do
+		rest = num % 2
+		t[#t + 1] = rest
+		num = (num - rest)/2
+	end
+	t = reverse(t)
+	local val = ''
+	for i = 1, #t do
+		val = val..t[i]
+	end
+	return tonumber(val)
+end
